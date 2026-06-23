083b4383b0c3504d1825911bd5ce58919c1be92e
areion91 <128209191+areion91@users.noreply.github.com>
2026-06-23 10:00:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+Script js
+
+const CLIENT_ID =
+"475553127157-nn2o47e7273qrk4rp46aljldoks35bbm.apps.googleusercontent.com";
+
+window.onload = () => {
+
+  google.accounts.id.initialize({
+
+    client_id: CLIENT_ID,
+
+    callback: handleCredentialResponse
+
+  });
+
+};
+
+document
+.getElementById("googleLogin")
+.addEventListener("click", () => {
+
+  google.accounts.id.prompt();
+
+});
+
+function handleCredentialResponse(response) {
+
+  const payload = JSON.parse(
+
+    atob(response.credential.split(".")[1])
+
+  );
+
+  const user = {
+
+    id: payload.sub,
+
+    name: payload.name,
+
+    email: payload.email,
+
+    picture: payload.picture
+
+  };
+
+  localStorage.setItem(
+
+    "s4s_user",
+
+    JSON.stringify(user)
+
+  );
+
+  window.location.href =
+
+    "./dashboard.html";
+
+}
